c6af003adb7daefabc640dc9f330e772d618f1a4
I Minds <codecookies@163.com>
2020-01-13 08:11:11 +0000

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use App\Models\User;
 use App\Policies\UserPolicy;
+use function foo\func;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Horizon\Horizon;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -30,6 +33,9 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        Horizon::auth(function ($request){
+            //是否是站长
+            return Auth::user()->hasRole('Founder');
+        });
     }
 }
